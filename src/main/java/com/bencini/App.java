8dc96799e2d2612c18e19a562ab00b5daef0dc06
lai2-01 <lai2-01@itismeucci.com>
2021-09-28 10:44:06 +0200

    Finita la classe ClientStr

--- src/main/java/com/bencini/App.java
+++ src/main/java/com/bencini/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ClientStr cliente = new ClientStr();
+        cliente.connetti();
+        cliente.comunica();
+
+        
     }
 }
